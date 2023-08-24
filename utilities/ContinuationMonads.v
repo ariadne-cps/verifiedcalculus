3c8eea8dbe491c6c925097e04cc53ca366dda2c9
Pieter Collins <pieter.collins@maastrichtuniversity.nl>
2023-08-24 19:03:15 +0200

    Updated continuation monads to explicitly parametrise on base type T.

--- utilities/ContinuationMonads.v
+++ utilities/ContinuationMonads.v
@@ -2,97 +2,100 @@ Require Import Coq.Logic.FunctionalExtensionality.
 
 Require Import Monads.
 
-Module Type ContinuationMonads.
+Section ContinuationMonads.
 
-Parameter T : Type.
+Definition Continuation (T : Type) (X : Type) := ( X-> T ) -> T.
 
-Definition M (X : Type) := ( X-> T ) -> T.
- 
-Definition pure {X : Type} (x : X) : (M X) :=
+Definition pure {T : Type} {X : Type} (x : X) : (Continuation T X) :=
   fun (phi : X -> T) => (phi x).
  
-Definition bind {X : Type} {Y : Type} (F : X -> M Y) (xi : M X) : (M Y) :=
+Definition bind {T : Type} {X : Type} {Y : Type}
+    (F : X -> Continuation T Y) (xi : Continuation T X) : (Continuation T Y) :=
   fun (psi : Y -> T) => xi (fun (x:X) => F x psi).
 
-Lemma bind_extensionality: forall {X1 X2 Y} (F1 : X1 -> M Y) (F2 : X2 -> M Y) (xi1 : M X1) (xi2 : M X2),
+Lemma bind_extensionality : forall {T} {X1 X2 Y} 
+    (F1 : X1 -> Continuation T Y) (F2 : X2 -> Continuation T Y) 
+    (xi1 : Continuation T X1) (xi2 : Continuation T X2),
   (forall (psi : Y -> T), bind F1 xi1 psi = bind F2 xi2 psi) -> bind F1 xi1 = bind F2 xi2.
-Proof. intros X1 X2 Y F1 F2 xi1 xi2. apply functional_extensionality. Qed.
+Proof. intros T X1 X2 Y F1 F2 xi1 xi2. apply functional_extensionality. Qed.
 
-Proposition left_identity  : forall {X} {Y} (F : X -> M Y) (x : X) (psi : Y->T), (bind F (pure x)) psi = F x psi.
+Proposition left_identity' : forall {T} {X Y} (F : X -> Continuation T Y) (x : X) (psi : Y->T), (bind F (pure x)) psi = F x psi.
 Proof.
-  intros X Y F x psi.
+  intros T X Y F x psi.
   unfold bind.
   unfold pure.
   reflexivity.
 Qed.
 
-Proposition right_identity  : forall {X} (xi : M X) (phi : X -> T), 
+Proposition right_identity' : forall {T} {X} (xi : Continuation T X) (phi : X -> T), 
   (bind (pure) xi) phi = xi phi.
 Proof.
-  intros X xi.
+  intros T X xi.
   intros phi.
   unfold bind.
   unfold pure.
   f_equal.
 Qed.
 
-Proposition associativity : forall {X} {Y} {Z} (xi:M X) (F:X->M Y) (G:Y->M Z) (theta : Z -> T),
+Proposition associativity' : forall {T} {X Y Z} (xi : Continuation T X) (F : X -> Continuation T Y) (G : Y -> Continuation T Z) (theta : Z -> T),
   bind G (bind F xi) theta = bind (fun x => bind G (F x)) xi theta.
 Proof.
-  intros X Y Z xi F G theta.
+  intros T X Y Z xi F G theta.
   unfold bind.
   reflexivity.
 Qed.
 
 
 
-Proposition left_identity_extensional : forall {X} {Y} (F : X -> M Y) (x : X), bind F (pure x) = F x.
-Proof. intros X Y F x. unfold bind, pure. apply functional_extensionality. intro psi. reflexivity. Qed.
+Proposition left_identity : forall {T} {X Y} (F : X -> Continuation T Y) (x : X), bind F (pure x) = F x.
+Proof. intros T X Y F x. unfold bind, pure. apply functional_extensionality. intro psi. reflexivity. Qed.
 
-Proposition right_identity_extensional : forall {X} (xi : M X),  bind pure xi = xi.
-Proof. intros X xi. unfold bind, pure. apply functional_extensionality. intro phi. f_equal. Qed.
+Proposition right_identity : forall {T} {X} (xi : Continuation T X),  bind pure xi = xi.
+Proof. intros T X xi. unfold bind, pure. apply functional_extensionality. intro phi. f_equal. Qed.
 
-Proposition associativity_extensional : forall {X} {Y} {Z} (xi:M X) (F:X->M Y) (G:Y->M Z),
+Proposition associativity : forall {T} {X Y Z} (xi : Continuation T X) (F : X -> Continuation T Y) (G : Y -> Continuation T Z),
   bind G (bind F xi) = bind (fun x => bind G (F x)) xi.
-Proof. intros X Y Z xi F G. unfold bind. reflexivity. Qed.
+Proof. intros T X Y Z xi F G. unfold bind. reflexivity. Qed.
 
 
-Definition lift {X:Type} {Y:Type} (f : X -> Y) (mu : M X) :=
+Definition lift {T} {X Y} (f : X -> Y) (mu : Continuation T X) :=
   bind (fun x => pure (f x)) mu.
 
-Definition right_product {X:Type} {Y:Type} (mu : M X) (nu : M Y) :=
+Definition right_product {T} {X Y} (mu : Continuation T X) (nu : Continuation T Y) :=
   bind ( fun x => ( bind (fun y => pure (pair x y)) nu ) ) mu.
 
-Definition left_product {X:Type} {Y:Type} (mu : M X) (nu : M Y) :=
+Definition left_product {T} {X Y} (mu : Continuation T X) (nu : Continuation T Y) :=
   bind ( fun y => ( bind (fun x => pure (pair x y)) mu ) ) nu.
 
 
 #[export]
-Instance Continuation_Monad : Monad M :=
+Instance Continuation_Monad : forall T:Type, Monad (Continuation T) :=
 {
-  Mpure := @pure;
-  Mbind := @bind;
+  Mpure := @pure T;
+  Mbind := @bind T;
 
   (* coherence conditions *)
-  Mleft_identity := @left_identity_extensional;
-  Mright_identity := @right_identity_extensional;
-  Massociativity := @associativity_extensional;
+  Mleft_identity := @left_identity T;
+  Mright_identity := @right_identity T;
+  Massociativity := @associativity T;
 }.
 
 
-(* Requires bind functional_extensionality *)
-Theorem lift_associative : forall {A:Type} {B:Type} {C:Type} (a : M A) (f:A->B) (g:B->C),
-  lift g (lift f a) = lift (fun x => g (f x)) a.
+Theorem lift_associativity : forall {T : Type} {X Y Z : Type} (xi : Continuation T X) (f : X -> Y) (g : Y -> Z),
+  lift g (lift f xi) = lift (fun x => g (f x)) xi.
 Proof.
-  intros A B C. intros a f g.
+  intros T X Y Z. intros xi f g.
   unfold lift.
-  rewrite -> associativity_extensional.
-  f_equal.
+  apply bind_extensionality.
+  intros theta.
+  unfold bind, pure.
+  reflexivity.
 Qed.
 
-Theorem product_pure_pure : forall {A:Type} {B:Type} (x : A) (y : B), left_product (pure x) (pure y) = right_product (pure x) (pure y).
+Theorem product_pure_pure : forall {T : Type} {X Y} (x : X) (y : Y), 
+  @left_product T X Y (pure x) (pure y) = right_product (pure x) (pure y).
 Proof.
-  intros A B. intros x b.
+  intros T X Y. intros x y.
   unfold left_product. unfold right_product.
   apply bind_extensionality. intro psi. 
   rewrite -> left_identity. rewrite -> left_identity.
@@ -100,20 +103,19 @@ Proof.
   reflexivity.
 Qed.
 
-(* Requires bind functional_extensionality *)
-Theorem product_pure_monad : forall {A:Type} {B:Type} (x : A) (b : M B), left_product (pure x) b = right_product (pure x) b.
+Theorem product_pure_monad : forall {T : Type} {X Y} (x : X) (eta : Continuation T Y), left_product (pure x) eta = right_product (pure x) eta.
 Proof.
-  intros A B. intros x b.
+  intros T X Y. intros x eta.
   unfold left_product. unfold right_product.
   apply bind_extensionality. intro psi. 
   rewrite -> left_identity.
   f_equal.
 Qed.
 
-(* Requires bind functional_extensionality *)
-Theorem product_monad_pure : forall {A:Type} {B:Type} (a : M A) (y : B), left_product a (pure y) = right_product a (pure y).
+Theorem product_monad_pure : forall {T : Type} {X Y} (xi : Continuation T X) (y : Y),
+  left_product xi (pure y) = right_product xi (pure y).
 Proof.
-  intros A B. intros a y.
+  intros T X Y. intros xi y.
   unfold left_product. unfold right_product.
   apply bind_extensionality. intro psi. 
   rewrite -> left_identity.
@@ -121,8 +123,11 @@ Proof.
 Qed.
 
 
+Section Experimental.
 
 (* An operator intended to be similar to logical disjunction. *)
+
+Parameter T : Type.
 Parameter ast : T -> T -> T.
 
 (* An relation intended to be some kind of equivalence on X. *)
@@ -130,11 +135,11 @@ Parameter X : Type.
 Parameter sim : X -> X -> T.
 
 (* An operator similar to the intersection of compact and closed sets. *)
-Definition oast : M X -> (X -> T) -> M X := 
+Definition oast : Continuation T X -> (X -> T) -> Continuation T X := 
   fun mu phi => fun psi : X -> T => mu (fun x : X => ast (phi x) (psi x)).
 
 (* An operator similar to the embedding of compact sets into closed sets for a Hausdorff space. *)
-Definition osim : M X -> (X -> T) :=
+Definition osim : Continuation T X -> (X -> T) :=
   fun mu => fun y => mu (fun x => sim x y).
   
 (* An interpretation of the whole space in the monad. *)
@@ -170,10 +175,11 @@ Proof.
   apply delta.
 Qed.
 
-Conjecture conditioning : forall {Y:Type} (sigma : M (X * Y) -> (X -> M Y)) (C : M (X * Y)),
+Conjecture conditioning : forall {Y:Type} (sigma : Continuation T (X * Y) -> (X -> Continuation T Y)) (C : Continuation T (X * Y)),
   (forall (x : X) (psi : Y -> T),
       ast ((lift fst C) (fun x'=>sim x x')) (sigma C x psi) = C ( fun (xy':X*Y) => ast (sim x (fst xy')) (psi (snd xy')) ) ) 
     -> Mright_skew (lift fst C) (sigma C) = C.
 
+End Experimental.
 
 End ContinuationMonads.
