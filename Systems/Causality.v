3b27196678e371a39d65be09a6c71aa7202d40c7
Pieter Collins <pieter.collins@maastrichtuniversity.nl>
2024-08-27 18:06:11 +0200

    IN_PROGRESS: Using sigma types for storing properties of unzipped traces.

--- Systems/Causality.v
+++ Systems/Causality.v
@@ -5,7 +5,7 @@
  *                 Master's Thesis Artificial Intelligence
  *                 Maastricht University
  *
- *  Copyright 2023 Pieter Collins
+ *  Copyright 2023-24 Pieter Collins
  *
  *  Definitions and results on causality (nonanticipatingness) of systems.
  *
@@ -50,41 +50,108 @@ Notation "x1 |≤ n |≡ x2" := (forall m, m <= n -> x1 m = x2 m) (at level 40).
 #[refine]
 Instance trace_equivalence {X} : Equivalence (@trace_equivalent X) := { }.
 Proof.
-  - intros u n. reflexivity.
-  - intros u1 u2 H n. symmetry. exact (H n).
-  - intros u1 u2 u3 H12 H23 n.
-    transitivity (u2 n). exact (H12 n). exact (H23 n).
+  - intros x12 n. reflexivity.
+  - intros x1 x2 H n. symmetry. exact (H n).
+  - intros x1 x2 u3 H12 H23 n.
+    transitivity (x2 n). exact (H12 n). exact (H23 n).
 Qed.
 
 
-Definition zip {T1 T2} (s1 : Tr T1) (s2 : Tr T2) := fun n => (s1 n, s2 n).
-Definition unzip {T1 T2} (s : Tr (T1*T2)) := (fun n => fst (s n), fun n => snd (s n)).
-Notation "( u1 ; u2 )" := (zip u1 u2) (at level 0).
+Definition zip {X1 X2} (x1 : Tr X1) (x2 : Tr X2) := fun n => (x1 n, x2 n).
+Definition unzip {X1 X2} (x12 : Tr (X1*X2)) := (fun n => fst (x12 n), fun n => snd (x12 n)).
+Notation "( x1 ; x2 )" := (zip x1 x2) (at level 0).
 
-Lemma zip_unzip {UA UD} : 
-  forall (u : Tr (UA*UD)),
-    zip (fst (unzip u)) (snd (unzip u)) ≡ u.
+Lemma zip_unzip {X1 X2} : 
+  forall (x12 : Tr (X1*X2)),
+    zip (fst (unzip x12)) (snd (unzip x12)) ≡ x12.
 Proof.
-  intros u n. unfold zip,unzip. simpl. 
+  intros x12 n. unfold zip,unzip. simpl. 
   now rewrite <- surjective_pairing.
 Qed.
 
-Lemma unzip_zip {UA UD} : 
-  forall (ua : Tr UA) (ud : Tr UD),
-    unzip (zip ua ud) = (ua,ud).
+Lemma unzip_zip {X1 X2} : 
+  forall (x1 : Tr X1) (x2 : Tr X2),
+    unzip (zip x1 x2) = (x1,x2).
 Proof.
-  intros ua ud. now unfold zip,unzip.
+  intros x1 ud. now unfold zip,unzip.
 Qed.
+
+(* Definition unzip_eqv {X1 X2} (x12 : Tr (X1*X2)) : { x12 : (Tr X1) * (Tr X2) | ( (fst x12;snd x12) ≡ x12 ) }
+  := exist _ (unzip x12) (zip_unzip x12). 
+*)
+
+Inductive sig_pair {A B : Type} (P : A -> B -> Prop) : Type :=
+  exist_pair : forall (x : A) (y : B), P x y -> sig_pair P.
+
+Definition unzip_eqv {X1 X2} (x12 : Tr (X1*X2)) : @sig_pair (Tr X1) (Tr X2) (fun x1 x2 => (x1;x2) ≡ x12 )
+  := exist_pair _ (fst (unzip x12)) (snd (unzip x12)) (zip_unzip x12). 
+
+Definition unzip_eq {X1 X2} (x12 : Tr (X1*X2))
+    : @sig_pair (Tr X1) (Tr X2) (fun x1 x2 => ((x1 = fst (unzip x12)) /\ x2 = snd (unzip x12)) /\ (x1;x2) ≡ x12 ).
+Proof.
+  set (x1 := fst (unzip x12)).
+  set (x2 := snd (unzip x12)).
+  assert ((x1 = fst (unzip x12) /\ x2 = snd (unzip x12)) /\ (x1;x2)≡x12) as p. {
+    split. split. reflexivity. reflexivity. now apply (zip_unzip x12). }
+  exact (exist_pair _ x1 x2 p).
+Qed.
+
+
+Definition unzip3_l {X1 X2 X3} (x123 : Tr ((X1*X2)*X3))
+  := ( ( fun n => fst (fst (x123 n)), fun n => snd (fst (x123 n)) ), fun n => snd (x123 n) ) .
+Lemma zip_unzip3_l {X1 X2 X3} :
+  forall (x123 : Tr ((X1*X2)*X3)),
+    x123 ≡ ((fst (unzip (fst (unzip x123))); 
+             snd (unzip (fst (unzip x123)))); 
+            snd (unzip x123)).
+Proof.
+  intros x n. unfold zip,unzip. simpl.
+  rewrite <- surjective_pairing. now rewrite <- surjective_pairing.
+Qed.
+Lemma zip_unzip3_l' {X1 X2 X3} : 
+  forall (x : Tr ((X1*X2)*X3)),
+    zip (zip (fst (unzip (fst (unzip x)))) (snd (unzip (fst (unzip x))))) (snd (unzip x)) ≡ x.
+Proof.
+  intros x n. unfold zip,unzip. simpl.
+  rewrite <- surjective_pairing. now rewrite <- surjective_pairing.
+Qed.
+Definition unzip3_l_eqv {X1 X2 X3} (x123 : Tr ((X1*X2)*X3))
+  : { x1x2x3 : ((Tr X1) * (Tr X2)) * (Tr X3) | ( ((fst (fst x1x2x3);snd (fst x1x2x3));snd x1x2x3) ≡ x123) }
+    := exist _ (unzip3_l x123) (zip_unzip3_l' x123). 
+
+Definition unzip3_r {X1 X2 X3} (x123 : Tr (X1*(X2*X3)))
+  := ( fun n => fst (x123 n), ( fun n => fst (snd (x123 n)), fun n => snd (snd (x123 n))) ) .
+Lemma zip_unzip3_r {X1 X2 X3} :
+  forall (x123 : Tr (X1*(X2*X3))),
+    x123 ≡ (fst (unzip x123);
+            (fst (unzip (snd (unzip x123))); 
+             snd (unzip (snd (unzip x123))))).
+Proof.
+  intros x n. unfold zip,unzip. simpl.
+  rewrite <- surjective_pairing. now rewrite <- surjective_pairing.
+Qed.
+Lemma zip_unzip3_r' {X1 X2 X3} : 
+  forall (x : Tr (X1*(X2*X3))),
+    zip (fst (unzip x)) (zip (fst (unzip (snd (unzip x)))) (snd (unzip (snd (unzip x))))) ≡ x.
+Proof.
+  intros x n. unfold zip,unzip. simpl.
+  rewrite <- surjective_pairing. now rewrite <- surjective_pairing.
+Qed.
+Definition unzip3_r_eqv {X1 X2 X3} (x123 : Tr (X1*(X2*X3)))
+  : { x1x2x3 : (Tr X1) * ((Tr X2) * (Tr X3)) | 
+      ( (fst x1x2x3;(fst (snd (x1x2x3));snd (snd x1x2x3))) ≡ x123) }
+    := exist _ (unzip3_r x123) (zip_unzip3_r' x123). 
+
 
 Lemma equal_projections : forall (A B : Type) (p1 p2 : A * B),
   forall (e : p1=p2), (fst p1 = fst p2) /\ (snd p1 = snd p2).
 Proof. intros A B p1 p2 e. now rewrite <- e. Qed.
   
-Lemma zip_prod {UA UD} : 
-  forall (ua ua' : Tr UA) (ud ud' : Tr UD),
-    ua ≡ ua' /\ ud ≡ ud' <-> (ua;ud) ≡ (ua';ud').
+Lemma zip_prod {X1 X2} : 
+  forall (x1 x1' : Tr X1) (x2 x2' : Tr X2),
+    x1 ≡ x1' /\ x2 ≡ x2' <-> (x1;x2) ≡ (x1';x2').
 Proof.
-  intros ua ua' ud ud'. 
+  intros x1 x1' x2 x2'. 
   split.
   - intro Huad. destruct Huad as [Hua Hud].
     unfold zip. intro n. f_equal. apply Hua. apply Hud.
@@ -94,81 +161,191 @@ Proof.
     -- intro n. specialize (Huad n). 
        apply (f_equal snd) in Huad. simpl in Huad. exact Huad.
 Qed.
-Lemma zip_prod_l {UA UD} : 
-  forall (ua : Tr UA) (ud ud' : Tr UD),
-    ud ≡ ud' -> (ua;ud) ≡ (ua;ud').
+Lemma zip_prod_l {X1 X2} : 
+  forall (x1 : Tr X1) (x2 x2' : Tr X2),
+    x2 ≡ x2' -> (x1;x2) ≡ (x1;x2').
 Proof. 
-  intros ua ud ud' Hud. apply zip_prod. split.
+  intros x1 x2 x2' Hud. apply zip_prod. split.
   reflexivity. assumption.
 Qed.
-Lemma zip_prod_r {UA UD} : 
-  forall (ua ua' : Tr UA) (ud : Tr UD),
-    ua ≡ ua' -> (ua;ud) ≡ (ua';ud).
+Lemma zip_prod_r {X1 X2} : 
+  forall (x1 x1' : Tr X1) (x2 : Tr X2),
+    x1 ≡ x1' -> (x1;x2) ≡ (x1';x2).
 Proof. 
-  intros ua ua' ud Hua. apply zip_prod. split.
+  intros x1 x1' x2 Hua. apply zip_prod. split.
   assumption. reflexivity.
 Qed.
 
-Lemma zip_prod_lt {U1 U2} : 
-  forall (u1 u1' : Tr U1) (u2 u2' : Tr U2) (n:nat),
-    u1 |< n |≡ u1' -> u2 |< n |≡ u2' -> (u1;u2) |< n |≡ (u1';u2').
+Lemma zip_prod_lt {X1 X2} : 
+  forall (x1 x1' : Tr X1) (x2 x2' : Tr X2) (n:nat),
+    x1 |< n |≡ x1' -> x2 |< n |≡ x2' -> (x1;x2) |< n |≡ (x1';x2').
 Proof.
-  intros u1 u1' u2 u2' n Hu1 Hu2 m Hmltn.
+  intros x1 x1' x2 x2' n Hx1 Hx2 m Hmltn.
   unfold zip, unzip. f_equal.
-  - exact (Hu1 m Hmltn).
-  - exact (Hu2 m Hmltn).
+  - exact (Hx1 m Hmltn).
+  - exact (Hx2 m Hmltn).
 Qed.
 
-Lemma unzip_prod {UA UD} :
-  forall (ua : Tr UA) (ud : Tr UD) (u : Tr (UA*UD)),
-    (ua,ud) = unzip u -> (ua;ud) ≡ u.
+Lemma unzip_prod {X1 X2} :
+  forall (x1 : Tr X1) (x2 : Tr X2) (x12 : Tr (X1*X2)),
+    (x1,x2) = unzip x12 -> (x1;x2) ≡ x12.
 Proof.
-  unfold zip, unzip. intros ua ud u H n.
+  unfold zip, unzip. intros x1 x2 x12 H n.
   apply injective_projections. 
   - apply (f_equal fst) in H. simpl in H. now rewrite -> H.
   - apply (f_equal snd) in H. simpl in H. now rewrite -> H. Qed.
-Lemma fst_unzip_proj {UA UD} 
-  {u : Tr (UA*UD)} {ua : Tr UA} {ud : Tr UD} : 
-    forall (e : (ua,ud) = unzip u), 
-      ua = fst (unzip u).
+Lemma fst_unzip_proj {X1 X2} 
+  {x12 : Tr (X1*X2)} {x1 : Tr X1} {x2 : Tr X2} : 
+    forall (e : (x1,x2) = unzip x12), 
+      x1 = fst (unzip x12).
 Proof. intro H. now apply (f_equal fst) in H. Qed.
-Lemma unzip_proj {UA UD} 
-  {u : Tr (UA*UD)} {ua : Tr UA} {ud : Tr UD} : 
-    forall (e : (ua,ud) = unzip u), 
-      ua = fst (unzip u) /\ ud = snd (unzip u).
+Lemma unzip_proj {X1 X2} 
+  {x12 : Tr (X1*X2)} {x1 : Tr X1} {x2 : Tr X2} : 
+    forall (e : (x1,x2) = unzip x12), 
+      x1 = fst (unzip x12) /\ x2 = snd (unzip x12).
 Proof.
   intro H. split.
   now apply (f_equal fst) in H. 
   now apply (f_equal snd) in H.
 Qed.
-Lemma fst_unzip_prod {UA UD} :
-  forall (u u' : Tr (UA*UD)),
-    u ≡ u' -> fst (unzip u) ≡ fst (unzip u').
+Lemma fst_unzip_prod {X1 X2} :
+  forall (x12 x12' : Tr (X1*X2)),
+    x12 ≡ x12' -> fst (unzip x12) ≡ fst (unzip x12').
 Proof.
-  intros u u' H n. unfold unzip. simpl. now rewrite <- (H n).
+  intros x12 x12' H n. unfold unzip. simpl. now rewrite <- (H n).
 Qed.
-Lemma snd_unzip_prod {UA UD} :
-  forall (u u' : Tr (UA*UD)),
-    u ≡ u' -> snd (unzip u) ≡ snd (unzip u').
+Lemma snd_unzip_prod {X1 X2} :
+  forall (x12 x12' : Tr (X1*X2)),
+    x12 ≡ x12' -> snd (unzip x12) ≡ snd (unzip x12').
 Proof.
-  intros u u' H n. unfold unzip. simpl. now rewrite <- (H n).
+  intros x12 x12' H n. unfold unzip. simpl. now rewrite <- (H n).
 Qed.
 
-Lemma eqv_le_lt_succ {U} : 
-  forall (u u' : Tr U) (n : nat),
-    u |≤ n |≡ u' -> u |< (S n) |≡ u'.
+Definition commute {X1 X2} (x12 : X1*X2) : (X2*X1) :=
+  let (x1,x2) := x12 in (x2,x1).
+Lemma commute_commute_id {X1 X2} : 
+  forall x12 : X1*X2, commute (commute x12) = x12. 
+Proof. 
+  intro x12. 
+  replace x12 with (fst x12, snd x12).
+  unfold commute. reflexivity.
+  now rewrite <- surjective_pairing.
+Qed.
+Definition commute_traces {X1 X2} (x12 : Tr (X1*X2)) : (Tr (X2*X1)) :=
+  fun n => commute (x12 n).
+Lemma commute_zip {X1 X2} : forall (x1 : Tr X1) (x2 : Tr X2),
+  commute_traces (x1;x2) = (x2;x1).
 Proof.
-  intros u u' n Hu m HmltSn.
+  intros x1 x2. unfold zip, commute_traces, commute. reflexivity. Qed.
+Lemma commute_commute_eqv {X1 X2} : 
+  forall x12 : Tr (X1*X2), commute_traces (commute_traces x12) ≡ x12. 
+Proof. intros x12 n. unfold commute_traces. apply commute_commute_id. Qed.
+
+Definition associate {Y1 Y2 Y3}
+  (y12_3 : Tr ((Y1*Y2)*Y3)) : Tr (Y1*(Y2*Y3)) :=
+    let (y12,y3) := unzip y12_3 in
+      let (y1,y2) := unzip y12 in
+        (y1;(y2;y3)).
+Definition unassociate {Y1 Y2 Y3}
+  (y1_23 : Tr (Y1*(Y2*Y3))) : Tr ((Y1*Y2)*Y3) :=
+    let (y1,y23) := unzip y1_23 in
+      let (y2,y3) := unzip y23 in
+        ((y1;y2);y3).
+Lemma associate_zip {Y1 Y2 Y3} :
+  forall (y1 : Tr Y1) (y2 : Tr Y2) (y3 : Tr Y3),
+    associate ((y1;y2);y3) = (y1;(y2;y3)).
+Proof. intros; reflexivity. Qed.
+Lemma unassociate_zip {Y1 Y2 Y3} :
+  forall (y1 : Tr Y1) (y2 : Tr Y2) (y3 : Tr Y3),
+    unassociate (y1;(y2;y3)) = ((y1;y2);y3).
+Proof. intros; reflexivity. Qed.
+Lemma associate_extensional {Y1 Y2 Y3} :
+  forall (y123 y123' : Tr ((Y1*Y2)*Y3)),
+    y123 ≡ y123' -> associate y123 ≡ associate y123'.
+Proof.
+  intros y123 y123' H123 n.
+  unfold associate, zip, unzip.
+  now rewrite <- (H123 n). 
+Qed.
+Lemma unassociate_extensional {Y1 Y2 Y3} :
+  forall (y123 y123' : Tr (Y1*(Y2*Y3))),
+    y123 ≡ y123' -> unassociate y123 ≡ unassociate y123'.
+Proof.
+  intros y123 y123' H123 n.
+  unfold unassociate, zip, unzip.
+  now rewrite <- (H123 n). 
+Qed.
+Lemma unassociate_associate_id {Y1 Y2 Y3} :
+  forall (y1 : Tr Y1) (y2 : Tr Y2) (y3 : Tr Y3),
+    unassociate (associate ((y1;y2);y3)) = ((y1;y2);y3).
+Proof.
+  intros y1 y2 y3. reflexivity.
+Qed.
+Lemma associate_unassociate_id {Y1 Y2 Y3} :
+  forall (y1 : Tr Y1) (y2 : Tr Y2) (y3 : Tr Y3),
+    associate (unassociate (y1;(y2;y3))) = (y1;(y2;y3)).
+Proof.
+  intros y1 y2 y3. reflexivity.
+Qed.
+Lemma unassociate_associate_eqv {Y1 Y2 Y3} :
+  forall (y123 : Tr ((Y1*Y2)*Y3)),
+    unassociate (associate y123) ≡ y123.
+Proof.
+  intros y123.
+  set (y12 := fst (unzip y123)).
+  set (y1 := fst (unzip y12)).
+  set (y2 := snd (unzip y12)).
+  set (y3 := snd (unzip y123)).
+  assert (y123 ≡ ((y1;y2);y3)) as E123 by apply zip_unzip3_l.
+  transitivity ((y1; y2); y3).
+  transitivity (unassociate (associate ((y1; y2); y3))).
+  - apply unassociate_extensional.
+    apply associate_extensional.
+    exact E123.
+  - now rewrite -> (unassociate_associate_id y1 y2 y3).
+  - symmetry.
+    exact E123.
+Qed.
+Lemma associate_unassociate_eqv {Y1 Y2 Y3} :
+  forall (y123 : Tr (Y1*(Y2*Y3))),
+    associate (unassociate y123) ≡ y123.
+Proof.
+  intros y123.
+  set (y23 := snd (unzip y123)).
+  set (y1 := fst (unzip y123)).
+  set (y2 := fst (unzip y23)).
+  set (y3 := snd (unzip y23)).
+  assert (y123 ≡ (y1;(y2;y3))) as E123 by apply zip_unzip3_r.
+  transitivity (y1; (y2; y3)).
+  transitivity (associate (unassociate (y1; (y2; y3)))).
+  - apply associate_extensional.
+    apply unassociate_extensional.
+    exact E123.
+  - rewrite -> (associate_unassociate_id y1 y2 y3).
+    reflexivity.
+  - symmetry.
+    exact E123.
+Qed.
+
+
+Lemma eqv_le_lt_succ {X} : 
+  forall (x x' : Tr X) (n : nat),
+    x |≤ n |≡ x' -> x |< (S n) |≡ x'.
+Proof.
+  intros x x' n Hu m HmltSn.
   set (Hmlen := (proj1 (Nat.lt_succ_r _ _)) HmltSn).
   exact (Hu m Hmlen).
 Qed.
 
-Lemma all_eqv_lt {U} :
-  forall (u u' : Tr U),
-    (forall n, u |< n |≡ u') -> u ≡ u'.
+Lemma all_eqv_lt {X} :
+  forall (x x' : Tr X),
+    (forall n, x |< n |≡ x') -> x ≡ x'.
 Proof.
-  intros u u' H n. exact (H (S n) n (Nat.lt_succ_diag_r n)).
+  intros x x' H n. exact (H (S n) n (Nat.lt_succ_diag_r n)).
 Qed.
+
+
+
 
 (* A behaviour is a function from input sequences to output sequences. *)
 Definition Behaviour (U:Type) (Y:Type) : Type := 
