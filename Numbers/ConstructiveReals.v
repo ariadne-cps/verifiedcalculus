4cf9d45e0c26ca6a548f889d086248f20c2b703a
Pieter Collins <pieter.collins@maastrichtuniversity.nl>
2026-03-26 15:41:28 +0100

    IN_PROGRESS: Experiments with constructive reals and comparisons.

---
+++ Numbers/ConstructiveReals.v
@@ -0,0 +1,43 @@
+(******************************************************************************
+ *  numbers/ConstructiveReals.v
+ *
+ *  Copyright 2026 Pieter Collins
+ *
+ ******************************************************************************)
+
+(*
+ * This file is part of the Verified Calculus Library.
+ *
+ * The Verified Calculus Library is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * The Verified Calculus Library is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * the Verified Calculus Library. If not, see <https://www.gnu.org/licenses/>.
+ *)
+
+
+Require Import Reals.
+Require Import Reals.Rbase.
+Require Import Reals.Rfunctions.
+Require Import Reals.Rbasic_fun.
+Require Import Reals.Rbasic_fun.
+Require Import Reals.Rdefinitions.
+
+Require Import Lra.
+
+Require Import Logic.Kleenean.
+Require Import DyadicFloat.
+
+Open Scope R_scope.
+
+
+
+
+Close Scope R_scope.
