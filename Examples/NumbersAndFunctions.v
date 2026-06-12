655ec823c5d78c1a09997b829f5c1eb64b916772
Pieter Collins <pieter.collins@maastrichtuniversity.nl>
2026-06-12 17:49:08 +0200

    IN_PROGRESS: Adding initial examples.

---
+++ Examples/NumbersAndFunctions.v
@@ -0,0 +1,35 @@
+(******************************************************************************
+ *  Examples/NumbersAndFunctions.v
+ *
+ *  Copyright 2026 Pieter Collins
+ *
+ *  Examples showing rounded, interval and polynomial model computations.
+ *
+ ******************************************************************************)
+
+(*
+ * This file is part of the Verified Calculus Library.
+ *
+ * The Verified Calculus Library is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * The Verified Calculus Library is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * the Verified Calculus Library. If not, see <https://www.gnu.org/licenses/>.
+ *)
+
+
+(* ---------------------------------------------------------------- *)
+
+
+Require Import Numbers.DyadicFloat.
+
+Definition x := Dyadic 1 3.
+
+Print x.
